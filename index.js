50a9884669868f4363cedd1bf427a6c8085af077
nsmag <natthakit.susanthitanon@gmail.com>
2016-05-15 12:21:41 +0700

    Update sundays

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const CommandType = SDK.CommandType;
 const inline = new SDK();
 
 inline.onCommand((type, payload, context) => {
-  const imageUrl = 'http://glitter.mthai.com/wordpress/wp-content/uploads/2015/08/mthai_photo_editor-37.png';
-  // const imageUrl = 'http://jeedna.com/wp-content/uploads/2016/01/m5.jpg';
+  const imageUrl1 = 'https://lh3.ggpht.com/qeItSe5LmBKPuTlr5g8eNgNoss1Qvh4GQRM0QG7AYrknUDE-ayl4l7owjaIj9YcSmLc=h900';
+  const imageUrl2 = 'http://glitter.mthai.com/wordpress/wp-content/uploads/2015/09/วันอาทิตย์.jpg';
+  const imageUrl3 = 'https://www.e-toyotaclub.net/site/Portals/0/Webboard/upload_image/Img073507.jpg';
 
-  context.sendImage(imageUrl, imageUrl);
+  const random = Math.random() * 2;
+
+  if (random < 0) {
+    context.sendImage(imageUrl, imageUrl1);
+  } else if (random < 1) {
+    context.sendImage(imageUrl, imageUrl2);
+  } else {
+    context.sendImage(imageUrl, imageUrl3);
+  }
 });
